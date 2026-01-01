b5c5559c9b8669d83b76fe69c8223455701140aa
Rafael García Prieto <rafael.garcia.prieto.tech@bbva.com>
2026-01-01 22:24:11 +0100

    chore(tooling): add eslint prettier and husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  env: {
+    es2022: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  overrides: [
+    {
+      files: ['apps/mobile/**/*.{ts,tsx}'],
+      env: {
+        browser: true,
+      },
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      plugins: ['@typescript-eslint', 'react', 'react-hooks', 'react-native'],
+      settings: {
+        react: {
+          version: 'detect',
+        },
+      },
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:react-native/all',
+        'prettier',
+      ],
+      rules: {
+        'react/react-in-jsx-scope': 'off',
+        'react-native/no-inline-styles': 'off',
+      },
+    },
+  ],
+};
