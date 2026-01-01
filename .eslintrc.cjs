a3fb4aff287c4d7397d9b84e34b154e7e8c08e7c
Rafael García Prieto <rafael.garcia.prieto.tech@bbva.com>
2026-01-01 22:26:35 +0100

    chore(style): tighten lint and ts rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -36,6 +36,11 @@ module.exports = {
       rules: {
         'react/react-in-jsx-scope': 'off',
         'react-native/no-inline-styles': 'off',
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+        ],
       },
     },
   ],
--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,5 +2,6 @@ module.exports = {
   singleQuote: true,
   trailingComma: 'all',
   printWidth: 100,
+  tabWidth: 2,
   semi: true,
 };
